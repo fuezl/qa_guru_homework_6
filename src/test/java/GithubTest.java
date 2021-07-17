80da73a3ca1713aebf8de3abb2a0b189463cd556
Александр <Aleksandr.Grachev@x5.ru>
2021-07-17 21:44:16 +0300

    тест без шагов и тест с шагами с помощью lambda

---
+++ src/test/java/GithubTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
+import static org.openqa.selenium.By.linkText;
+
+public class GithubTest {
+
+    String REPOSITORY = "eroshenkoam/allure-example";
+    String TAB = "Issues";
+    String ISSUE_NUMBER = "68";
+
+    @Test
+    void testRepositoryIssue() {
+        //Arrange
+        open("https://github.com");
+
+        //Act
+        $(".header-search-input").setValue("eroshenkoam/allure-example").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $(".js-repo-nav").$(byText("Issues")).click();
+
+        //Assert
+        $(withText("#68")).should(exist);
+    }
+
+    @Test
+    void testRepositoryIssueWithLambdaSteps() {
+        //Arrange
+        step("Открываем главную страницу Github", () ->
+                open("https://github.com"));
+
+        //Act
+        step(format("Ищем репозиторий %s", REPOSITORY), () ->
+                $(".header-search-input").setValue(REPOSITORY).submit());
+        step("Открываем репозиторий", () ->
+                $(linkText("eroshenkoam/allure-example")).click());
+        step(format("Переходим на вкладку %s", TAB), () ->
+                $(".js-repo-nav").$(byText("Issues")).click());
+
+        //Assert
+        step(format("Проверяем существование issue c номером %s", ISSUE_NUMBER), () ->
+                $(withText(ISSUE_NUMBER)).should(exist));
+    }
+}
